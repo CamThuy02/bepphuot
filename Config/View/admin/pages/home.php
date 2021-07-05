76e36fa135b77bd49c937c51169e9f3d07e274a2
Lingg209 <linhlinh.lh71@gmail.com>
2021-07-05 12:37:38 +0700

    fix loi chuyen muc cha

--- Config/View/admin/pages/home.php
+++ Config/View/admin/pages/home.php
@@ -8,7 +8,16 @@
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card overview-item--c1 text-white mb-4">
-                                    <div class="card-body">CHUYÊN MỤC</div>
+                                    <div class="card-body">CHUYÊN MỤC CHA</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="?controller=listParentCategory">Chi tiết</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card overview-item--c1 text-white mb-4">
+                                    <div class="card-body">CHUYÊN MỤC CON</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="?controller=listCategory">Chi tiết</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
