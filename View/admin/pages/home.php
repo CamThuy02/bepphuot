f8a8b73cc9ec7d1d3fe4971986b257242e22e988
Lingg209 <linhlinh.lh71@gmail.com>
2021-07-05 14:11:02 +0700

    fix cha

--- View/admin/pages/home.php
+++ View/admin/pages/home.php
@@ -7,8 +7,17 @@
                         </ol>
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
+                                <div class="card overview-item--c4 text-white mb-4">
+                                    <div class="card-body">CHUYÊN MỤC CHA</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="?controller=listParentCategory">Chi tiết</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
                                 <div class="card overview-item--c1 text-white mb-4">
-                                    <div class="card-body">CHUYÊN MỤC</div>
+                                    <div class="card-body">CHUYÊN MỤC CON</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="?controller=listCategory">Chi tiết</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
@@ -37,7 +46,7 @@
                                 </div>
                             </div>
                             <div class="col-xl-3 col-md-6">
-                                <div class="card overview-item--c4 text-white mb-4">
+                                <div class="card overview-item--c3 text-white mb-4">
                                     <div class="card-body">BÌNH LUẬN</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">Chi tiết</a>
