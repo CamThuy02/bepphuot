97db574459c6d8497597ff4a1164ce2de515617a
Tuyetanh090720 <ngothituyetanhbmt@gmail.com>
2021-07-06 12:28:43 +0700

    xoa

--- View/client/pages/category/postCategory.php
+++ View/client/pages/category/postCategory.php
@@ -190,33 +190,6 @@
 
             </div>
             <?php } ?>
-            <div class="col-lg-12 col-md-6 col-sm-12">
-                <div class="media media-none--md mb-30">
-                    <div class="position-relative width-40">
-                        <a href="single.html" class="img-opacity-hover">
-                            <img src="https://cdn.dayphache.edu.vn/wp-content/uploads/2020/07/cac-mon-do-uong-cho-be.jpg"
-                                alt="news" class="img-fluid">
-                        </a>
-                    </div>
-                    <div class="media-body p-mb-none-child media-margin30">
-                        <div class="post-date-dark">
-                            <ul>
-                                <li>
-                                    <span>
-                                        <i class="fa fa-calendar" aria-hidden="true"></i>
-                                    </span>04 tháng 5, 2021
-                                </li>
-                            </ul>
-                        </div>
-                        <h3 class="title-semibold-dark size-lg mb-15">
-                            <a href="single.html">Cách Làm Mocktail Nổi Tiếng Dành Cho Trẻ Em.</a>
-                        </h3>
-                        <p>Lựa chọn đồ uống cho trẻ em thì không phải là một điều dễ dàng. Giờ bạn không
-                            phải lo lắng, vì hôm nay chúng tôi sẽ giới thiệu một số mocktail cho trẻ em.
-                        </p>
-                    </div>
-                </div>
-            </div>
         </div>
         <!--seemore -->
         <center>
